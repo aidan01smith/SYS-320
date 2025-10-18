5db57c27142097c8fd4052e4cf6788c08dec6cc3
aidan01smith <aidan.smith01@mymail.champlain.edu>
2025-10-18 19:23:18 -0400

    challenge three commit

---
+++ MidTerm/Challenge3.ps1
@@ -0,0 +1,39 @@
+Import-Module -Name "C:\Users\champuser\Documents\SYS-320\MidTerm\Challenge1.ps1"
+Import-Module -Name "C:\Users\champuser\Documents\SYS-320\MidTerm\Challenge2.ps1"
+
+# used chadbaldwin.net for reference on filtering logs
+
+function Get-IOCMatches {
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [array]$Logs,
+
+        [Parameter(Mandatory=$true)]
+        [array]$IOCs
+    )
+
+    #establish a blank array to sort through
+    $matches = @()
+
+    foreach($log in $Logs) {
+        foreach($ioc in $IOCs) {
+            #recognize the pattern to match the html to the log file
+            if($log.Page -like "*$($ioc.Pattern)*") {
+                $matches += $log
+                break
+            }
+        }
+    }
+
+    return $matches
+
+}
+
+#using the same trick as before to find the page and look through the logs to display the sus files
+$iocs = Get-IOC -URL "http://10.0.17.47/IOC.html"
+$logs = Get-ApacheLogs -LogPath "C:\Users\champuser\Documents\SYS-320\MidTerm\access.log"
+$suspiciousLogs = Get-IOCMatches -Logs $logs -IOCs $iocs
+
+
+$suspiciousLogs | Format-Table IP, Time, Method, Page, Protocol, Response, Referrer -AutoSize
