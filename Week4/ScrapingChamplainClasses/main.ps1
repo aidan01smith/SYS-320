76fbd811b7117b63815f3dedca8c372c8a7fc0c6
aidan01smith <aidan.smith01@mymail.champlain.edu>
2025-11-05 16:44:49 -0500

    sorting things out

--- Week4/ScrapingChamplainClasses/main.ps1
+++ Week4/ScrapingChamplainClasses/main.ps1
@@ -1,7 +1,10 @@
+<#
+
 . (Join-Path $PSScriptRoot ChamplainScrape.ps1)
 
 $table = gatherClasses
 $FullTable = daysTranslator $table
+
 
 
 $ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
@@ -16,3 +19,23 @@ $ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -ilike "SYS*") -o
 
 $FullTable | Where {$_.Instructor -in $ITSInstructors.Instructor}`
            | Group-Object "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending
+#>
+
+. (Join-Path $PSScriptRoot ChamplainScrape.ps1)
+
+$table = gatherClasses
+$FullTable = daysTranslator $table
+
+$SISInstructors = $FullTable | Where-Object { ($_."Class_Code" -ilike "SYS*") -or `
+                                              ($_."Class_Code" -ilike "NET*") -or `
+                                              ($_."Class_Code" -ilike "SEC*") -or `
+                                              ($_."Class_Code" -ilike "FOR*") -or `
+                                              ($_."Class_Code" -ilike "CSI*") -or `
+                                              ($_."Class_Code" -ilike "DAT*") } `
+                              | Select-Object "Instructor" `
+                              | Sort-Object "Instructor" -Unique
+
+$FullTable | Where-Object { $_.Instructor -in $SISInstructors.Instructor } `
+           | Group-Object "Instructor" `
+           | Select-Object Count, Name `
+           | Sort-Object Count -Descending
